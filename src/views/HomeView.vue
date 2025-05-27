c3e6b5c9f9c2f0014383dda5e63a6c8062c056fd
André Santiago <82945862+Dresovense@users.noreply.github.com>
2025-05-27 20:51:18 +0200

    Home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,34 @@
 
 <template>
   <main>
-    <h1>Lausanne Ancestry</h1>
+    <div class="analysis">
+      <h1 class="analysis_title">Lausanne Ancestry</h1>
+      <div>
+        La mobilité sociale est un phénomène dans lequel une personne étant née dans une certaine classe sociale en
+        sort.
+        Ce phénomène est en lien avec la transformation du travail (l’indicateur de la classe sociale au sein de ce
+        travail),
+        ayant eu lieu lors du XIXe siècle, avec l'avènement de la révolution industrielle, faisant passer Lausanne d’une
+        ville agricole à une ville industrielle.
+        <br>
+        <br>
+        Ce projet vise donc à explorer les relations entre parents et enfants, du point de vue du travail de chaque
+        acteur.
+        De plus, une interface interactive permet d’explorer les différentes familles trouvées dans les recensements.
+      </div>
+    </div>
   </main>
 </template>
+
+<style>
+.analysis_title{
+  margin-bottom: 2%;
+  text-align: center;
+  font-size: larger;
+  font-weight: bold;
+}
+
+.analysis {
+  margin: 5%;
+}
+</style>
